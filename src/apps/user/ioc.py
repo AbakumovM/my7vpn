4f1e199349a422a0f1ea6c8ec68e07b1d2deea63
Mikhail Abakumov <abakumovms7@yandex.ru>
2026-04-29 08:02:19 +0500

    feat: register AdminView in UserProvider

--- src/apps/user/ioc.py
+++ src/apps/user/ioc.py
@@ -1,9 +1,11 @@
 from dishka import Provider, Scope, provide
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.apps.user.adapters.admin_view import SQLAlchemyAdminView
 from src.apps.user.adapters.gateway import SQLAlchemyUserGateway
 from src.apps.user.adapters.view import SQLAlchemyUserView
 from src.apps.user.application.interactor import UserInteractor
+from src.apps.user.application.interfaces.admin_view import AdminView
 from src.apps.user.application.interfaces.gateway import UserGateway
 from src.apps.user.application.interfaces.view import UserView
 from src.infrastructure.database.uow import SQLAlchemyUoW
@@ -19,6 +21,10 @@ class UserProvider(Provider):
     @provide
     def get_view(self, session: AsyncSession) -> UserView:
         return SQLAlchemyUserView(session)
+
+    @provide
+    def get_admin_view(self, session: AsyncSession) -> AdminView:
+        return SQLAlchemyAdminView(session)
 
     @provide
     def get_interactor(self, gateway: UserGateway, uow: SQLAlchemyUoW) -> UserInteractor:
